e7e4e0053e52083778e94a068285f3234f84a456
Ichikyube <ichikyube@gmail.com>
2025-10-30 00:50:38 +0700

    cleaning

--- src/hooks/useJobApplication.ts
+++
@@ -1,8 +0,0 @@
-import { useMutation } from '@tanstack/react-query'
-import { applyToJob } from '@/lib/apiApplications'
-
-export function useJobApplication() {
-  return useMutation({
-    mutationFn: ({ jobId, formData }: any) => applyToJob(jobId, formData),
-  })
-}
