a32ddf1b1b372d3dfd28ebc3f852910dac7bcd5c
Ichikyube <ichikyube@gmail.com>
2025-11-04 08:35:26 +0700

    cleaning

--- jest.config.js
+++ jest.config.js
@@ -44,7 +44,7 @@ const customJestConfig = {
     '^.+\\.(ts|tsx)?$': 'ts-jest', 
     '^.+\\.(js|jsx)$': 'babel-jest',
   },
-  testPathIgnorePatterns: ["<rootDir>/tests/setup.ts"], 
+  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/tests/setup.ts"], 
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
