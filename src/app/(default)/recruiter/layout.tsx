65f03089d5f4b0281ebac410ed49bab9e6f5684a
Ichikyube <ichikyube@gmail.com>
2025-11-19 20:07:15 +0700

    restore applicant api route

--- src/app/(default)/recruiter/layout.tsx
+++ src/app/(default)/recruiter/layout.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { ProtectedRoute } from "@/components/layout/protected-route"
 import { useAuthStore } from "@/stores/auth-store";
-import { X } from "lucide-react";
 import { useRouter } from "next/navigation";
 import type React from "react"
 export default function DefaultLayout({
@@ -13,15 +12,8 @@ export default function DefaultLayout({
   const user = useAuthStore((state) => state.user);
   const userRole = user?.role;
   
-  if (userRole !== 'RECRUITER') { // Assuming 'RECRUITER' is the role type
-    router.push('/jobs');
-    return (
-      <div className="flex flex-col items-center justify-center min-h-[calc(100vh-64px)] p-8 text-center text-red-500">
-        <X className="h-10 w-10 mb-4" />
-        <p className="text-xl font-bold">Access Denied</p>
-        <p className="mt-2">You must be logged in as a Recruiter to view this page.</p>
-      </div>
-    );
+  if (userRole && userRole !== 'RECRUITER') {
+    return router.push('/jobs');
   }
   return (
     <ProtectedRoute>
