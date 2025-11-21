9e77839fbc7edf65c3fb388c0e4eaee20943f49c
Ichikyube <ichikyube@gmail.com>
2025-11-22 01:57:47 +0700

    test apply

--- src/app/api/jobs/[jobId]/analytics/route.ts
+++ src/app/api/jobs/[jobId]/analytics/route.ts
@@ -1,4 +1,4 @@
-// app/api/jobs/[id]/analytics/route.ts
+import "server-only";
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "@/lib/prisma";
 import { auth } from "@/auth";
